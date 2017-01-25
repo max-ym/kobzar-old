b8f89866819a562d028929e2b0f2566f719113a8
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-26 00:41:26 +0200

    New crate attributes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#![crate_name = "kernel"]
+#![crate_type = "staticlib"]
+#![no_std]
+
 /// The starting point of kernel Rust code execution.
 /// Before this point runs some initial assembly code that initializes
 /// the environment where Rust code can start performing.
-
 fn main() -> ! {
     /* Things to be done:
      *
