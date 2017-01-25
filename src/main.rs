356744ddef2410c9df9c6f5d491485217ca852b3
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-25 18:56:50 +0200

    Added comment with developement plan

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@
 /// the environment where Rust code can start performing.
 
 fn main() -> ! {
+    /* Things to be done:
+     *
+     * Initialize CCS model.
+     * Start multi-threading.
+     * Load memory manager.
+     *
+     * Start initial kernel graphics server.
+     * Start initial kernel logger.
+     * Start file system servers.
+     * Run startup script (if any is found).
+     * Start kernel terminal.
+     */
 
     loop {};
 }
