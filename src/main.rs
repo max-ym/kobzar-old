ef08d66d2fced0946cc73a8af40bc9c75ecee151
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-25 18:49:15 +0200

    New main.rs

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+/// The starting point of kernel Rust code execution.
+/// Before this point runs some initial assembly code that initializes
+/// the environment where Rust code can start performing.
+
+fn main() -> ! {
+
+    loop {};
+}
