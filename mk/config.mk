f8466a47bcdc86ea0de641933af78cde910d5164
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-26 23:30:37 +0200

    Moved main variables to separate makefile

---
+++ mk/config.mk
@@ -0,0 +1,49 @@
+# Main variables used in rules. Defines the structure of build directories.
+
+# Source code directory
+SRCDIR ?= ./src/
+
+# Main rust file to start kernel compilation from
+MAINRS ?= $(SRCDIR)main.rs
+
+##########
+# Operating system architecture.
+# Change if needed. Currently only x86_64 is valid.
+ARCH = x86_64
+
+ifeq ($(ARCH), x86_64)
+	TRIPLE ?= x86_64-elf-
+else
+	$(error Unsupported architecture: $(ARCH))
+endif
+##########
+
+# Build directory
+BUILDDIR ?= ./build/$(ARCH)/
+
+# Downloads directory. Any related packages from internet are stored there.
+DOWNDIR ?= ./build/downloads/
+
+# Object build directory
+OBJBDIR := $(BUILDDIR)obj/
+
+# ISO root directory
+ISODIR := $(BUILDDIR)iso/
+
+# Rust libraries directory
+RUSTLIBDIR := ./rustlibs/
+
+# All rust sources list
+RSRCLIST := $(shell find $(SRCDIR) -type f -name '*.rs')
+
+# Configuration scripts
+CONFIGDIR  ?= ./config/$(ARCH)/
+GRUBCFG    := $(CONFIGDIR)grub.cfg
+LINKSCRIPT := $(CONFIGDIR)link.ld
+TARGETSPEC := $(CONFIGDIR)target.json
+
+# Rust compiler, flags and combination.
+RUSTC ?= rustc
+RUSTF ?= -O --cfg arch__$(ARCH) --target=$(TARGETSPEC)
+RUSTCF := $(RUSTC)
+RUSTCF += $(RUSTF)
