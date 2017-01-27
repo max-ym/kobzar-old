e9fa746b434699486a6b2500d8ff125b5c88c92e
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-27 15:00:15 +0200

    Decomposed rules

--- mk/binutils.mk
+++ mk/binutils.mk
@@ -7,20 +7,26 @@ BINUTILSDIR ?= $(DOWNDIR)binutils/
 BINUTILSARCHIVENAME ?= binutils-2.27.tar.bz2
 
 # Name of dearchivated folder.
-BINUTILSDEARCHDIR ?= binutils-2.27
+BINUTILSDEARCHDIR ?= binutils-2.27/
 
 # Path to download binutils from.
 BINUTILSLINK ?= https://ftp.gnu.org/gnu/binutils/$(BINUTILSARCHIVENAME)
 
-# Rule to download and build binutils.
-$(BINUTILSDIR):
+# Rule to download binutils.
+$(BINUTILSDIR)$(BINUTILSARCHIVENAME):
 	@echo 'Binutils archive is being downloaded.'
-	@mkdir -p $@
-	@wget -P $@ $(BINUTILSLINK)
+	@mkdir -p $(BINUTILSDIR)
+	@wget -P $(BINUTILSDIR) $(BINUTILSLINK)
+
+# Rule to decompress the binutils archive.
+$(BINUTILSDIR)../$(BINUTILSDEARCHDIR): $(BINUTILSDIR)$(BINUTILSARCHIVENAME)
 	@echo 'Binutils archive is being decompressed...'
-	@tar jxf $@$(BINUTILSARCHIVENAME) --directory $@..
-	@rm $@$(BINUTILSARCHIVENAME)
-	@mkdir $@../$(BINUTILSDEARCHDIR)/build/
+	@tar jxf $< --directory $(BINUTILSDIR)..
+
+# Rule to download and build binutils.
+$(BINUTILSDIR): $(BINUTILSDIR)../$(BINUTILSDEARCHDIR)
+	@mkdir $@build/
 	@echo 'Binutils is being configured and built.'
-	@cd $@../$(BINUTILSDEARCHDIR)/ && ../$(BINUTILSDEARCHDIR)/configure --target=$(TRIPLE) --prefix="$(shell pwd)/$@" --with-sysroot --disable-nls --disable-werror && make && make install
-	@rm -r $@../$(BINUTILSDEARCHDIR)/
+	@cd $@../$(BINUTILSDEARCHDIR) && ../$(BINUTILSDEARCHDIR)configure --target=$(TRIPLE) --prefix="$(shell pwd)/$@" --with-sysroot --disable-nls --disable-werror && make && make install
+	@rm $@$(BINUTILSARCHIVENAME)
+	@rm -r $@../$(BINUTILSDEARCHDIR)
