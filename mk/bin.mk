6a123e9bed69344b53319f2fd2ad1685ae0ad619
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-27 13:02:24 +0200

    New rule to download and build binutils. Not tested

--- mk/bin.mk
+++ mk/bin.mk
@@ -1,1 +1,25 @@
- 
+# Rules to build binary executable file with kernel.
+
+# Binutils directory.
+BINUTILSDIR ?= $(DOWNDIR)binutils/
+
+# Name of downloaded binutils archive.
+BINUTILSARCHIVENAME ?= binutils-2.27.tar.bz2
+
+# Name of dearchivated folder.
+BINUTILSDEARCHDIR ?= binutils-2.27
+
+# Path to download binutils from.
+BINUTILSLINK ?= https://ftp.gnu.org/gnu/binutils/$(BINUTILSARCHIVENAME)
+
+# Rule to download and build binutils.
+$(BINUTILSDIR):
+	@echo 'Binutils archive is being downloaded.'
+	@mkdir -p $@
+	@wget -P $@ $(BINUTILSLINK)
+	@tar zxf $@$(BINUTILSARCHIVENAME) --directory $@..
+	@rm $@$(BINUTILSARCHIVENAME)
+	@mkdir $@build/
+	@cd $@build/ && ../$(BINUTILSDEARCHDIR)/configure --target=$(TRIPLE) --prefix="$@" --with-sysroot --disable-nls --disable-werror && make && make install
+	@rm -r $@build/
+	@rm -r $@$(BINUTILSDEARCHDIR)
