0ab3914c4ea38f38c4b9d57655c83f6f3a5c838f
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-27 18:59:17 +0200

    Fix binutils unneeded recompilation

--- mk/binutils.mk
+++ mk/binutils.mk
@@ -12,24 +12,19 @@ BINUTILSDEARCHDIR ?= binutils-2.27/
 # Path to download binutils from.
 BINUTILSLINK ?= https://ftp.gnu.org/gnu/binutils/$(BINUTILSARCHIVENAME)
 
-# Rule to download binutils.
-$(BINUTILSDIR)$(BINUTILSARCHIVENAME):
+# Rule to download and build binutils.
+$(BINUTILSDIR):
 	@echo 'Binutils archive is being downloaded.'
 	@mkdir -p $(BINUTILSDIR)
 	@wget -P $(BINUTILSDIR) $(BINUTILSLINK)
-
-# Rule to decompress the binutils archive.
-$(BINUTILSDIR)../$(BINUTILSDEARCHDIR): $(BINUTILSDIR)$(BINUTILSARCHIVENAME)
 	@echo 'Binutils archive is being decompressed...'
-	@tar jxf $< --directory $(BINUTILSDIR)..
-
-# Rule to download and build binutils.
-$(BINUTILSDIR): $(BINUTILSDIR)../$(BINUTILSDEARCHDIR)
-	@mkdir $@build/
+	@tar jxf $@$(BINUTILSARCHIVENAME) --directory $(BINUTILSDIR)..
+	@mkdir -p $@build/
 	@echo 'Binutils is being configured and built.'
 	@cd $@../$(BINUTILSDEARCHDIR) && ../$(BINUTILSDEARCHDIR)configure --target=$(TRIPLE) --prefix="$(shell pwd)/$@" --with-sysroot --disable-nls --disable-werror && make && make install
 	@rm $@$(BINUTILSARCHIVENAME)
 	@rm -r $@../$(BINUTILSDEARCHDIR)
+	@rm -r $@build/
 
 # Linker
 LD ?= $(BINUTILSDIR)bin/$(TRIPLE)-ld
