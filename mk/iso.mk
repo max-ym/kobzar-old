d302dbf76283b94bea3d324c4bb022a2ca93f862
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-27 20:33:34 +0200

    New rules to build ISO image

--- mk/iso.mk
+++ mk/iso.mk
@@ -1,1 +1,19 @@
 # Rules to build an ISO image with OS.
+
+# ISO image location.
+ISO ?= $(BUILDDIR)image.iso
+
+# Directory where ISO file structure is composed.
+ISOBDIR ?= $(BUILDDIR)iso/
+
+iso: $(ISO)
+
+$(ISO): $(KERNELBIN) $(GRUBCFG)
+	@mkdir -p $(ISOBDIR)boot/grub
+	@ln -f $(GRUBCFG) $(ISOBDIR)boot/grub/grub.cfg
+	@ln -f $(KERNELBIN) $(ISOBDIR)boot/kernel.bin
+	# Remove old image if any
+	@rm -f $(KERNELISO)
+	@grub-mkrescue -o $@ $(ISOBDIR)
+	# Clean
+	@rm -r $(ISOBDIR)
