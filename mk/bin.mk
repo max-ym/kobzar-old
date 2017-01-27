9b9237f532ed1655e3fe5742576eea8c4dd5f79c
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-27 19:00:41 +0200

    New rule for linking objects

--- mk/bin.mk
+++ mk/bin.mk
@@ -1,1 +1,13 @@
 # Rules to build binary executable file with kernel.
+
+# All object files list.
+OBJS := $(ASMOBJ) $(KERNOBJ)
+
+# Kernel executable binary file.
+KERNELBIN := $(BUILDDIR)kernel.bin
+
+bin: $(KERNELBIN)
+
+# Rule to build a binary executable kernel file.
+$(KERNELBIN): $(LD) $(OBJS) $(LINKSCRIPT)
+	$(LD) -o $@ $(LDFLAGS) $(OBJS)
