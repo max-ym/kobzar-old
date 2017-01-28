ae96532ce2acd55619732c3f4251c8efa7b3eaa6
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-28 23:45:54 +0200

    New macro to define MSR structure and basic functions

--- src/asm/x86_64/msr.rs
+++ src/asm/x86_64/msr.rs
@@ -1,8 +1,8 @@
 /// Info read from MSR.
 #[derive(Clone, Copy)]
 pub struct Info {
-    pub eax     : u64,
-    pub edx     : u64,
+    pub rax     : u64,
+    pub rdx     : u64,
 }
 
 impl Info {
@@ -19,9 +19,12 @@ impl Info {
             : "{ecx}"(id)
         );
 
-        Info { eax:a, edx:d }
+        Info { rax:a, rdx:d }
     }
 
+    /// See 'read_by_id'. Note, that this function generally must not be used.
+    /// It is more appropriate to use relevant 'read' function in the structure
+    /// that represents the desired MSR.
     pub unsafe fn read(msr: Msr) -> Info {
         Self::read_by_id(msr as u32)
     }
@@ -31,4 +34,33 @@ impl Info {
 #[repr(u32)]
 pub enum Msr {
     ApicBase        = 27,
+}
+
+macro_rules! derive_info {
+    ($x:ident) => (
+        #[derive(Clone, Copy)]
+        pub struct $x {
+            rax     : u64,
+            rdx     : u64,
+        }
+
+        impl Into<Info> for $x {
+
+            fn into(self) -> Info {
+                Info { rax: self.rax, rdx: self.rdx }
+            }
+        }
+
+        impl $x {
+
+            /// Read this given MSR. Note that if it is not defined
+            /// in the processor, General Protection fault will be
+            /// rised. You need to ensure that processor supports this MSR.
+            pub unsafe fn read() -> Self {
+                let info = Info::read(Msr::$x);
+                // Convert the Info structure to correspond to given MSR.
+                ::core::mem::transmute(info)
+            }
+        }
+    );
 }
