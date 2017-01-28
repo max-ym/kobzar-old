9d0ac2d45060bd1776bdf9e0ba0f016ff1cd4e29
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-28 23:32:16 +0200

    New fn to read MSR by it's id

--- src/asm/x86_64/msr.rs
+++ src/asm/x86_64/msr.rs
@@ -1,6 +1,24 @@
 /// Info read from MSR.
 #[derive(Clone, Copy)]
 pub struct Info {
-    pub eax     : u32,
-    pub edx     : u32,
+    pub eax     : u64,
+    pub edx     : u64,
+}
+
+impl Info {
+
+    /// Get data in MSR by it's code. This function is unsafe as
+    /// some MSRs may not be defined and so this call will cause
+    /// General Protection fault. Ensure that MSR with given ID actually
+    /// exists.
+    pub unsafe fn read_by_id(id: u32) -> Info {
+        let (a, d);
+        asm!(
+            "rdmsr"
+            : "={rax}"(a), "={rdx}"(d)
+            : "{ecx}"(code)
+        );
+
+        Info { eax:a, edx:d }
+    }
 }
