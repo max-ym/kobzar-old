737280ee27a3f4834f150428edf3d70225856bb3
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-28 19:09:38 +0200

    New InfoType enum and fn 'get' in Info struct

--- src/asm/x86_64/cpuid.rs
+++ src/asm/x86_64/cpuid.rs
@@ -6,3 +6,37 @@ pub struct Info {
     pub ecx     : u32,
     pub edx     : u32,
 }
+
+/// Enum that stores all valid CPUID query codes.
+#[repr(u32)]
+#[derive(Clone, Copy)]
+pub enum InfoType {
+    VendorString    = 0x00,
+    Features        = 0x01,
+    TLB             = 0x02,
+    Serial          = 0x03,
+
+    IntelExtended       = 0x8000_0000,
+    IntelFeatures       = 0x8000_0001,
+    IntelBrandString    = 0x8000_0002,
+    IntelBrandStringMore= 0x8000_0003,
+    IntelBrandStringEnd = 0x8000_0004,
+}
+
+impl Info {
+
+    /// Run CPUID instruction to query information.
+    pub fn get(info: InfoType) -> Self {
+        use self::InfoType::*;
+        let (a, b, c, d);
+
+        let i = info as u32;
+        unsafe { asm!(
+            "cpuid"
+            : "={eax}"(a), "={ebx}"(b), "={ecx}"(c), "={edx}"(d)
+            : "{eax}"(i)
+        ); }
+
+        Info { eax:a, ebx:b, ecx:c, edx:d }
+    }
+}
