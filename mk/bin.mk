f62ad3b6caebf911f3b0cefe0a2de51d121bde01
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-28 03:13:02 +0200

    Added missing link flags

--- mk/bin.mk
+++ mk/bin.mk
@@ -6,6 +6,10 @@ OBJS := $(ASMOBJ) $(KERNOBJ)
 # Kernel executable binary file.
 KERNELBIN := $(BUILDDIR)kernel.bin
 
+LDFLAGS := -T $(LINKSCRIPT)
+LDFLAGS += --gc-sections
+LDFLAGS += -z max-page-size=0x1000
+
 bin: $(KERNELBIN)
 
 # Rule to build a binary executable kernel file.
