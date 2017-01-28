9c0ac0434c5daa99821a9627f697c570bb068d9c
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-28 14:47:14 +0200

    Listed Intel defined Interrupt Vector

--- src/interrupts/x86_64/mod.rs
+++ src/interrupts/x86_64/mod.rs
@@ -9,3 +9,30 @@ pub struct IDT {
     /// The array of all 256 gates of the IDT.
     pub gates:  [IDTGate; 256],
 }
+
+/// The list of architecture defined interrupt vectors.
+/// For more information see Intel System Programming Guide.
+pub enum InterruptVector {
+
+    DivideError     = 0,
+    DebugException  = 1,
+    NMI             = 2,
+    Breakpoint      = 3,
+    Overflow        = 4,
+    BOUNDRange      = 5,
+    InvalidOpcode   = 6,
+    NoMath          = 7,
+    DoubleFault     = 8,
+
+    InvalidTSS          = 10,
+    SegmentNotPresent   = 11,
+    StackSegmentFault   = 12,
+    GeneralProtection   = 13,
+    PageFault           = 14,
+
+    MathFault               = 16,
+    AlignmentCheck          = 17,
+    MachineCheck            = 18,
+    SIMDException           = 19,
+    VirtualizationException = 20,
+}
