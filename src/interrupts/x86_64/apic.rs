4963b82675c248344dc69d5d67f3a545414c62c1
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-29 00:46:39 +0200

    New fns to create LocalApic struct instance + minor changes

--- src/interrupts/x86_64/apic.rs
+++ src/interrupts/x86_64/apic.rs
@@ -1,9 +1,30 @@
 /// Local APIC related functions and data.
 pub struct LocalApic {
-    // TODO
+    /// Local APIC MSR access.
+    msr     : ::asm::msr::ApicBase,
 }
 
 impl LocalApic {
+
+    /// Create new structure to hold information about Local APIC state.
+    /// Note that this will read MSR that may be unimplemented which will
+    /// cause General Protection fault.
+    ///
+    /// You can use 'new' that will check if APIC is actually available
+    /// before any read to MSR.
+    pub unsafe fn unsafe_new() -> LocalApic {
+        let msr = ::asm::msr::ApicBase::read();
+
+        LocalApic { msr:msr }
+    }
+
+    pub fn new() -> Option<LocalApic> {
+        if !Self::is_present() {
+            None
+        } else {
+            unsafe { Some(Self::unsafe_new()) }
+        }
+    }
 
     pub fn is_present() -> bool {
         ::asm::cpuid::Features::get().check_local_apic_presence()
