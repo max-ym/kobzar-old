09cd2fbd009440ab0db9f751d8a78b4b65769a13
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-29 06:36:31 +0200

    Renamed fn

--- src/asm/x86_64/cpuid.rs
+++ src/asm/x86_64/cpuid.rs
@@ -166,7 +166,7 @@ impl Features {
     }
 
     /// Check if Local APIC is present.
-    pub fn local_apic_present(&self) -> bool {
+    pub fn local_apic_is_present(&self) -> bool {
         self.info.edx & 0b0000_0000_0000_0000_0000_0001_0000_0000 != 0
     }
 }
