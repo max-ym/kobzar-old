204993cdae448dc0bee2e6a25dcb4168689c18b6
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-29 04:16:02 +0200

    New statics and functions to work with them.

--- src/features.rs
+++ src/features.rs
@@ -1,7 +1,27 @@
 use ::asm::cpuid;
 use ::asm::msr;
 
-static CPUID_VENDOR_STRING  : Option<cpuid::VendorString>   = None;
-static CPUID_FEATURES       : Option<cpuid::Features>       = None;
-static CPUID_TLB            : Option<cpuid::Tlb>            = None;
-static CPUID_SERIAL         : Option<cpuid::Serial>         = None;
+// TODO multi-threading safety.
+
+static mut CPUID_VENDOR_STRING  : Option<cpuid::VendorString>   = None;
+static mut CPUID_FEATURES       : Option<cpuid::Features>       = None;
+static mut CPUID_TLB            : Option<cpuid::Tlb>            = None;
+static mut CPUID_SERIAL         : Option<cpuid::Serial>         = None;
+
+static mut MSR_APIC_BASE        : Option<msr::ApicBase>         = None;
+
+pub static mut apic_base        : fn() -> &'static msr::ApicBase
+                                = init_apic_base;
+
+fn init_apic_base() -> &'static msr::ApicBase {
+    // TODO properly deal with unsafety!
+    unsafe {
+    MSR_APIC_BASE   = Some(msr::ApicBase::read());
+    apic_base       = get_apic_base;
+    apic_base()
+    }
+}
+
+fn get_apic_base() -> &'static msr::ApicBase {
+    unsafe { &MSR_APIC_BASE.as_ref().unwrap() }
+}
