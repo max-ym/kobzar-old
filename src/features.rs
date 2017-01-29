8fe0cb84cd7d0867f8cc19428529aaa18efb9e82
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-29 03:47:35 +0200

    Removed mod features

--- src/features.rs
+++ src/features.rs
@@ -1,2 +1,7 @@
 use ::asm::cpuid;
 use ::asm::msr;
+
+static CPUID_VENDOR_STRING  : Option<cpuid::VendorString>   = None;
+static CPUID_FEATURES       : Option<cpuid::Features>       = None;
+static CPUID_TLB            : Option<cpuid::Tlb>            = None;
+static CPUID_SERIAL         : Option<cpuid::Serial>         = None;
