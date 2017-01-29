5ecda50f6bf08da5cbbc0862874ec965b7ada63f
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-29 07:00:06 +0200

    Removed all CPUID features as they are not so important and hard to retireve to store in memory all the time

--- src/features.rs
+++ src/features.rs
@@ -3,46 +3,11 @@ use ::asm::msr;
 
 // TODO multi-threading safety.
 
-static mut CPUID_VENDOR_STRING  : Option<cpuid::VendorString>   = None;
-static mut CPUID_FEATURES       : Option<cpuid::Features>       = None;
-static mut CPUID_TLB            : Option<cpuid::Tlb>            = None;
-static mut CPUID_SERIAL         : Option<cpuid::Serial>         = None;
-
 static mut MSR_APIC_BASE        : Option<msr::ApicBase>         = None;
-
-fn get_apic_base() -> &'static msr::ApicBase {
-    unsafe { &MSR_APIC_BASE.as_ref().unwrap() }
-}
-
-fn load_vendor_string() {
-    unsafe { CPUID_VENDOR_STRING = Some(cpuid::VendorString::get()) }
-}
-
-fn load_features_cpuid() {
-    unsafe { CPUID_FEATURES = Some(cpuid::Features::get()) }
-}
-
-fn load_tlb() {
-    unsafe { CPUID_TLB = Some(cpuid::Tlb::get()) }
-}
-
-fn load_serial() {
-    unsafe { CPUID_SERIAL = Some(cpuid::Serial::get()) }
-}
 
 // Do not run this unless you ensured that this MSR is present.
 fn load_apic_base() {
     unsafe { MSR_APIC_BASE = Some(msr::ApicBase::read()) }
-}
-
-/// Get CPUID Features.
-///
-/// # Safety
-/// The function will fail only if this function gets runned before feature
-/// list is loaded. Do not run the function before its corresponding
-/// static variable is set to some value.
-pub fn features() -> cpuid::Features {
-    unsafe { CPUID_FEATURES.unwrap() }
 }
 
 /// Early setup of feature lists.
@@ -53,25 +18,10 @@ pub mod setup {
 
     /// Load list with features that are received from CPUID.
     pub fn load_feature_lists(logger: &mut Logger) {
-        logger.println("Buffering basic CPUID feature list:");
-
-        logger.println(" * Vendor String");
-        load_vendor_string();
-
-        logger.println(" * Features");
-        load_features_cpuid();
-
-        logger.println(" * TLB");
-        load_tlb();
-
-        logger.println(" * Serial");
-        load_serial();
-
-        // Start loading MSRs:
-        logger.println("\nBuffering basic MSR feature list:");
+        logger.println("Buffering basic MSR feature list:");
 
         // Check if APIC_BASE MSR is present.
-        if features().local_apic_is_present() {
+        if cpuid::Features::get().local_apic_is_present() {
             logger.println(" * APIC Base");
             load_apic_base();
         } else {
