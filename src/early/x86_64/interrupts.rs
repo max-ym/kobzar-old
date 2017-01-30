0a4c084ceb8450c0ea0467bcdbbb2d317030b9d4
Max Naumch <max.naumch@gmail.com>
2017-01-30 04:15:36 +0200

    Added to main initialization code APIC initialization

--- src/early/x86_64/interrupts.rs
+++ src/early/x86_64/interrupts.rs
@@ -1,3 +1,9 @@
+use ::interrupts::apic;
+
 pub fn setup_interrupts(logger: &mut ::early::Logger) {
-    unimplemented!();
+    use super::LoggerTrait;
+    logger.println("Starting Local APIC, if any available.");
+    apic::init();
+
+    // TODO
 }
