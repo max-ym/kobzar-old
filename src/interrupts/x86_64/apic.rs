cee8fab8746755225e930265c306b2d5e008917b
Max Naumch <max.naumch@gmail.com>
2017-01-30 04:00:36 +0200

    Updated APIC module design + new initialization fn

--- src/interrupts/x86_64/apic.rs
+++ src/interrupts/x86_64/apic.rs
@@ -1,15 +1,30 @@
-/// The base address of Local APIC registers mapped to RAM.
-/// This struct allows to update the value atomically. It is designed
-/// to be used as a singleton.
-pub struct BaseAddress {
-    lock    : ::core::sync::atomic::AtomicBool,
-    addr    : u64,
-}
+use ::asm::msr::ApicBase;
+use ::asm::cpuid;
+use ::asm::lowmem::APIC_BASE_ADDRESS;
 
-pub static mut BASE_ADDRESS: BaseAddress
-        = BaseAddress {
-            // This initial value is invalid. At system startup must be
-            // loaded with the right value.
-            addr: 0,
-            lock: ::core::sync::atomic::AtomicBool::new(false)
-        };
+/// Shows if Local APIC is available on current machine.
+/// The initial value is not correct before module initialization.
+static mut APIC_PRESENT: bool = false;
+
+/// Initialize the APIC module.
+pub fn init() {
+    // Check if Local APIC is availabale.
+    if cpuid::Features::get().local_apic_is_present() {
+        unsafe { APIC_PRESENT = true; }
+    } else {
+        unsafe { APIC_PRESENT = false; }
+
+        // Nothing to initalize as there is no Local APIC.
+        return;
+    }
+
+    // Get APIC Base MSR. It is safe to do so as we checked that Local APIC
+    // is really present.
+    let mut msr = unsafe { ApicBase::read() };
+
+    // Move the APIC registers to Low Memory.
+    msr.set_apic_base(APIC_BASE_ADDRESS);
+
+    // Save the MSR changes.
+    unsafe { msr.write() };
+}
