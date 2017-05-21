a3a754a59f2e346921289b491359f2673933a032
Max Naumch <max.naumch@gmail.com>
2017-05-21 20:01:13 +0300

    New Service struct fields

--- src/ccs/mod.rs
+++ src/ccs/mod.rs
@@ -1,6 +1,12 @@
 /// CCS Service handle.
 struct Service {
-    // TODO
+
+    /// The name of the service. Used to find a service to link to.
+    name : * const char,
+
+    /// Function address in object memory to run when service is requested.
+    /// The code pointer is 32-bit wide.
+    func : u32,
 }
 
 /// CCS Object handle.
