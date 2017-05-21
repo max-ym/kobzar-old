5ce16f995a3c2b0da8d7f66687998c4bde8b620e
Max Naumch <max.naumch@gmail.com>
2017-05-21 18:02:55 +0300

    Fixed errors

--- mk/libasm-x86_64.mk
+++ mk/libasm-x86_64.mk
@@ -1,22 +1,24 @@
 # Rules to download and build asm-x86_64 library for kernel.
 
-ASM-x86_64-GITREPO := https://github.com/max-ym/rust-asm-x86_64.git
+# Git repository name
+ASM-X86_64-GITNAME := rust-asm-x86_64
 
-OBJASM-x86_64-DOWNDIR := $(DOWNDIR)asm-x86_64/
+# Destination directory name
+ASM-X86_64-NAME := asm-x86_64
 
-OBJASM-x86_64 := $(OBJBDIR)libasm-x86_64.rlib
+ASM-X86_64-GITREPO := https://github.com/max-ym/$(ASM-X86_64-GITNAME).git
+
+OBJASM-X86_64-DOWNDIR := $(DOWNDIR)$(ASM-X86_64-NAME)/
+
+OBJASM-X86_64 := $(OBJBDIR)libasm-x86_64.rlib
 
 # Rule to download asm-x86_64 library. This library is used in kernel
 # source code.
-$(OBJASM-x86_64-DOWNDIR):
+$(OBJASM-X86_64-DOWNDIR):
 	@echo 'New asm-x86_64 library will be downloaded from github.'
 	@mkdir -p $(DOWNDIR)
-	@git -C $(DOWNDIR) clone $(ASM-x86_64-GITREPO) --branch master
+	@git -C $(DOWNDIR) clone $(ASM-X86_64-GITREPO) --branch master
+	@mv $(DOWNDIR)$(ASM-X86_64-GITNAME) $(OBJASM-X86_64-DOWNDIR)
 
-$(OBJASM-x86_64): $(OBJASM-x86_64-DOWNDIR)
-	$(RUSTCF) --crate-type rlib --crate-name asm_x86_64 $(OBJASM-x86_64-DOWNDIR)src/lib.rs
-
-asm-x86_64-rm:
-	@rm -rf $(OBJASM-x86_64-DOWNDIR)
-
-asm-x86_64: $(OBJASM-x86_64)
+$(OBJASM-X86_64): $(OBJASM-X86_64-DOWNDIR) $(TARGETSPEC)
+	$(RUSTCF) --out-dir=$(OBJBDIR) --emit=link,dep-info --extern core=$(OBJCORE) $(OBJASM-X86_64-DOWNDIR)src/lib.rs
