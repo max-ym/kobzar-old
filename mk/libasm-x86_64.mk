d745f8a7a6e7608e5a67def5e11b23e9d3b57ba9
Max Naumch <max.naumch@gmail.com>
2017-05-21 15:13:10 +0300

    Added rules to libasm-x86_64.mk

--- mk/libasm-x86_64.mk
+++ mk/libasm-x86_64.mk
@@ -1,3 +1,22 @@
-# Rules to build asm-x86_64 library for kernel.
+# Rules to download and build asm-x86_64 library for kernel.
 
-# TODO
+ASM-x86_64-GITREPO := https://github.com/max-ym/rust-asm-x86_64.git
+
+OBJASM-x86_64-DOWNDIR := $(DOWNDIR)asm-x86_64/
+
+OBJASM-x86_64 := $(OBJBDIR)libasm-x86_64.rlib
+
+# Rule to download asm-x86_64 library. This library is used in kernel
+# source code.
+$(OBJASM-x86_64-DOWNDIR):
+	@echo 'New asm-x86_64 library will be downloaded from github.'
+	@mkdir -p $(DOWNDIR)
+	@git -C $(DOWNDIR) clone $(ASM-x86_64-GITREPO) --branch master
+
+$(OBJASM-x86_64): $(OBJASM-x86_64-DOWNDIR)
+	$(RUSTCF) --crate-type rlib --crate-name asm_x86_64 $(OBJASM-x86_64-DOWNDIR)src/lib.rs
+
+asm-x86_64-rm:
+	@rm -rf $(OBJASM-x86_64-DOWNDIR)
+
+asm-x86_64: $(OBJASM-x86_64)
