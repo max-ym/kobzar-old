2e4dd8ffeddfbcba9e77192ca1983ca35a9c2aab
Maxym Naumchyk <max.naumch@gmail.com>
2017-05-23 02:39:10 +0300

    New structs in 'ccs' mod

--- src/ccs/mod.rs
+++ src/ccs/mod.rs
@@ -2,7 +2,7 @@
 struct Service {
 
     /// The name of the service. Used to find a service to link to.
-    name : * const char,
+    name : * const str,
 
     /// Function address in object memory to run when service is requested.
     /// The code pointer is 32-bit wide.
@@ -10,6 +10,40 @@ struct Service {
 }
 
 /// CCS Object handle.
-struct Object {
-    // TODO
+struct Object<'a> {
+
+    /// The name of the object. Used to find a service to link to.
+    name : &'a str,
+
+    /// List of all services that this object provides to external network.
+    service_list : ServiceList<'a>,
+
+    // List of all sub-objects.
+    sub_list : ObjectList<'a>,
+}
+
+struct ServiceList<'a> {
+    top : Option<&'a ServiceListNode<'a>>,
+}
+
+struct ServiceListNode<'a> {
+
+    /// The actual service.
+    service : Service,
+
+    /// Next node, if any.
+    next : Option<&'a ServiceListNode<'a>>,
+}
+
+struct ObjectList<'a> {
+    top: Option<&'a ObjectListNode<'a>>,
+}
+
+struct ObjectListNode<'a> {
+
+    /// The actual service.
+    object : Object<'a>,
+
+    /// Next node, if any.
+    next : Option<&'a ServiceListNode<'a>>,
 }
