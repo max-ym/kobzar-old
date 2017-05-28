f1cdd0ad846ded6c213ae35db2001750c3dfcf50
Maxym Naumchyk <max.naumch@gmail.com>
2017-05-28 13:53:32 +0300

    Made all fns with Ptr struct immutable by using Cell on 'addr' field

--- src/ccs/x86_64/mod.rs
+++ src/ccs/x86_64/mod.rs
@@ -1,10 +1,11 @@
 use ::ccs;
 use ::early::ccs::*;
 use ::mem::map::{CCS_BASIC_SETUP_ADDRESS, CCS_BASIC_SETUP_ADDRESS_END};
+use core::cell::Cell;
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 struct Ptr {
-    addr    : usize,
+    addr    : Cell<usize>,
     limit   : usize,
 }
 
@@ -13,57 +14,60 @@ impl Ptr {
     /// Create new pointer struct for given address.
     pub fn new_heap(addr: usize, limit: usize) -> Self {
         Ptr {
-            addr    : addr,
+            addr    : Cell::new(addr),
             limit   : limit,
         }
     }
 
     pub fn as_object_ptr<'a>(&self) -> *mut ccs::Object<'a> {
-        unsafe { ::core::mem::transmute(self.addr) }
+        unsafe { ::core::mem::transmute(self.addr.get()) }
     }
 
     pub fn as_service_node_ptr<'a>(&self) -> *mut ccs::ServiceListNode<'a> {
-        unsafe { ::core::mem::transmute(self.addr) }
+        unsafe { ::core::mem::transmute(self.addr.get()) }
     }
 
     pub fn as_object_node_ptr<'a>(&self) -> *mut ccs::ObjectListNode<'a> {
-        unsafe { ::core::mem::transmute(self.addr) }
+        unsafe { ::core::mem::transmute(self.addr.get()) }
     }
 
-    pub fn skip_object(&mut self) {
-        self.addr += ::core::mem::size_of::<ccs::Object>();
+    pub fn skip_object(&self) {
+        let size = ::core::mem::size_of::<ccs::Object>();
+        self.addr.set(self.addr.get() + size);
     }
 
-    pub fn skip_service_node(&mut self) {
-        self.addr += ::core::mem::size_of::<ccs::ServiceListNode>();
+    pub fn skip_service_node(&self) {
+        let size = ::core::mem::size_of::<ccs::ServiceListNode>();
+        self.addr.set(self.addr.get() + size);
     }
 
-    pub fn skip_object_node(&mut self) {
-        self.addr += ::core::mem::size_of::<ccs::ObjectListNode>();
+    pub fn skip_object_node(&self) {
+        let size = ::core::mem::size_of::<ccs::ObjectListNode>();
+        self.addr.set(self.addr.get() + size);
     }
 
     pub fn addr(&self) -> usize {
-        self.addr
+        self.addr.get()
     }
 
     pub fn limit(&self) -> usize {
         self.limit
     }
 
-    pub fn next_object_ptr<'a>(&mut self) -> *mut ccs::Object<'a> {
+    pub fn next_object_ptr<'a>(&self) -> *mut ccs::Object<'a> {
         let object_ptr = self.as_object_ptr();
         self.skip_object();
         object_ptr
     }
 
-    pub fn next_object_node_ptr<'a>(&mut self)
+    pub fn next_object_node_ptr<'a>(&self)
             -> *mut ccs::ObjectListNode<'a> {
         let list_node_ptr = self.as_object_node_ptr();
         self.skip_object_node();
         list_node_ptr
     }
 
-    pub fn next_service_node_ptr<'a>(&mut self)
+    pub fn next_service_node_ptr<'a>(&self)
             -> *mut ccs::ServiceListNode<'a> {
         let list_node_ptr = self.as_service_node_ptr();
         self.skip_service_node();
@@ -71,13 +75,13 @@ impl Ptr {
     }
 
     pub fn exceeds(&self, addr: usize) -> bool {
-        self.addr >= addr
+        self.addr.get() >= addr
     }
 }
 
 trait Allocate : Sized {
 
-    fn get_allocated(ptr: &mut Ptr) -> Option<*mut Self> {
+    fn get_allocated(ptr: &Ptr) -> Option<*mut Self> {
         let size = ::core::mem::size_of::<Self>();
 
         if ptr.addr() + size > ptr.limit() {
@@ -87,9 +91,9 @@ trait Allocate : Sized {
         }
     }
 
-    unsafe fn alloc_next_ptr(ptr: &mut Ptr) -> *mut Self;
+    unsafe fn alloc_next_ptr(ptr: &Ptr) -> *mut Self;
 
-    fn allocate_ptr(ptr: &mut Ptr) -> *mut Self {
+    fn allocate_ptr(ptr: &Ptr) -> *mut Self {
         let option = Self::get_allocated(ptr);
 
         if let Some(val) = option {
@@ -99,11 +103,11 @@ trait Allocate : Sized {
         }
     }
 
-    fn allocate_mut(ptr: &mut Ptr) -> &mut Self {
+    fn allocate_mut(ptr: &Ptr) -> &mut Self {
         unsafe { &mut *Self::allocate_ptr(ptr) }
     }
 
-    fn allocate_and_move(mut self, ptr: &mut Ptr) -> &mut Self {
+    fn allocate_and_move(mut self, ptr: &Ptr) -> &mut Self {
         let reference = Self::allocate_mut(ptr);
         *reference = self;
         reference
@@ -112,21 +116,21 @@ trait Allocate : Sized {
 
 impl<'a> Allocate for ccs::Object<'a> {
 
-    unsafe fn alloc_next_ptr(ptr: &mut Ptr) -> *mut Self {
+    unsafe fn alloc_next_ptr(ptr: &Ptr) -> *mut Self {
         ptr.next_object_ptr()
     }
 }
 
 impl<'a> Allocate for ccs::ServiceListNode<'a> {
 
-    unsafe fn alloc_next_ptr(ptr: &mut Ptr) -> *mut Self {
+    unsafe fn alloc_next_ptr(ptr: &Ptr) -> *mut Self {
         ptr.next_service_node_ptr()
     }
 }
 
 impl<'a> Allocate for ccs::ObjectListNode<'a> {
 
-    unsafe fn alloc_next_ptr(ptr: &mut Ptr) -> *mut Self {
+    unsafe fn alloc_next_ptr(ptr: &Ptr) -> *mut Self {
         ptr.next_object_node_ptr()
     }
 }
