30ebf298e97e132c8bc0b6ba79197f6dc7c42841
Maxym Naumchyk <max.naumch@gmail.com>
2017-05-30 01:14:03 +0300

    New 'alloc' mod

--- src/mem/x86_64/mod.rs
+++ src/mem/x86_64/mod.rs
@@ -3,3 +3,6 @@ pub mod gdt;
 
 // The memory map of kernel image. Module doc is placed in 'map.rs' sources.
 pub mod map;
+
+/// Memory allocator for system. Manages pages of memory for further use.
+pub mod alloc;
