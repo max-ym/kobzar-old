6a0590c38fe78b4699db9b1660d20b3dd55b0ab6
Maxym Naumchyk <max.naumch@gmail.com>
2017-05-30 01:35:17 +0300

    New fn 'pop'

--- src/mem/x86_64/alloc/p2m.rs
+++ src/mem/x86_64/alloc/p2m.rs
@@ -7,3 +7,21 @@ pub struct Stack {
     /// The count of addresses on the stack.
     count   : u32,
 }
+
+impl Stack {
+
+    /// Remove last value from the stack and return it.
+    fn pop(&mut self) -> Option<u64> {
+        if self.count == 0 {
+            return None;
+        }
+
+        unsafe {
+            let val = *self.top;
+            self.top = self.top.offset(-1);
+            self.count -= 1;
+
+            Some(val)
+        }
+    }
+}
