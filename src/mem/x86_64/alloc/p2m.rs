c9b6d729a0a47cc1009f3ee4c973b5ee3c034d9a
Maxym Naumchyk <max.naumch@gmail.com>
2017-05-30 01:43:03 +0300

    New struct 'Page2mHandle'

--- src/mem/x86_64/alloc/p2m.rs
+++ src/mem/x86_64/alloc/p2m.rs
@@ -2,16 +2,24 @@
 pub struct Stack {
 
     /// Top of the stack.
-    top     : *mut u64,
+    top     : *mut Page2mHandle,
 
     /// The count of addresses on the stack.
     count   : u32,
 }
 
+#[derive(Clone, Copy)]
+/// 2MiB page handle.
+pub struct Page2mHandle {
+
+    /// Address of a page.
+    addr    : u64
+}
+
 impl Stack {
 
     /// Remove last value from the stack and return it.
-    pub fn pop(&mut self) -> Option<u64> {
+    pub fn pop(&mut self) -> Option<Page2mHandle> {
         if self.count == 0 {
             return None;
         }
@@ -26,11 +34,22 @@ impl Stack {
     }
 
     /// Add new value onto the stack.
-    pub fn push(&mut self, val: u64) {
+    pub fn push(&mut self, val: Page2mHandle) {
         unsafe {
             self.count += 1;
             self.top = self.top.offset(1);
             *self.top = val;
         }
+    }
+}
+
+impl Page2mHandle {
+
+    pub fn new(addr: u64) -> Self {
+        Page2mHandle { addr : addr }
+    }
+
+    pub fn addr(&self) -> u64 {
+        self.addr
     }
 }
