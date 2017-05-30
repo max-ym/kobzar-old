fc7afb81104d03705ccf6bd74812d489ef6aa189
Maxym Naumchyk <max.naumch@gmail.com>
2017-05-31 00:41:47 +0300

    New struct ServiceData

--- src/ccs/x86_64/mod.rs
+++ src/ccs/x86_64/mod.rs
@@ -1,3 +1,12 @@
 /// Module to setup x86_64 architecture CCS.
 mod setup;
 pub use self::setup::setup;
+pub use super::Service;
+
+/// All information needed to execute the service. Memory pages, register file
+/// stack etc.
+pub struct ServiceData {
+
+    /// General information about service.
+    base    : Service,
+}
