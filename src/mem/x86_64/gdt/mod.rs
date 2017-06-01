7f669a76af6180a2fcda0afe467458a4334fef19
Max Naumch <max.naumch@gmail.com>
2017-06-01 16:06:03 +0300

    New Gdtr struct

--- src/mem/x86_64/gdt/mod.rs
+++ src/mem/x86_64/gdt/mod.rs
@@ -1,2 +1,12 @@
 /// Descriptors module.
 pub mod desc;
+
+/// Global Descriptor Table Register.
+#[repr(packed)]
+pub struct Gdtr {
+    /// Address of the GDT.
+    addr    : u64,
+
+    /// Limit. Number of entries of GDT.
+    limit   : u16,
+}
