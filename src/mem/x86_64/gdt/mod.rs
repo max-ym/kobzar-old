a6d651f5b42b0b55cacd0fb9fdec1bd1dc70ed2c
Max Naumch <max.naumch@gmail.com>
2017-06-01 16:10:51 +0300

    New fn

--- src/mem/x86_64/gdt/mod.rs
+++ src/mem/x86_64/gdt/mod.rs
@@ -1,12 +1,30 @@
 /// Descriptors module.
 pub mod desc;
 
-/// Global Descriptor Table Register.
+/// Global Descriptor Table Register value.
 #[repr(packed)]
-pub struct Gdtr {
+pub struct GdtrValue {
     /// Address of the GDT.
     addr    : u64,
 
     /// Limit. Number of entries of GDT.
     limit   : u16,
+}
+
+impl GdtrValue {
+
+    /// Write current value to the GDTR.
+    pub unsafe fn write(&self) {
+        unimplemented!();
+    }
+
+    /// Read current value from the GDTR.
+    pub fn read(&mut self) {
+        unimplemented!();
+    }
+
+    /// Create GdtrValue struct from current value in GDTR.
+    pub fn new_from_gdtr() -> Self {
+        unimplemented!();
+    }
 }
