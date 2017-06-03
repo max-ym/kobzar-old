8f55ddc84e615af531a8fdf23d9ec77c52785647
Maxym Naumchyk <max.naumch@gmail.com>
2017-06-04 00:20:21 +0300

    Changed memory map

--- src/mem/arch/x86_64/map.rs
+++ src/mem/arch/x86_64/map.rs
@@ -5,11 +5,10 @@
 //! >=80000 - may not all be used.
 //!
 //! Memory map of the kernel in selected region:
-//! 00000:003FF - free
+//! 00000:000FF - free
+//! 00100:003FF - CCS basic setup temporary memory.
 //! 00400:004FF - BIOS Data Area.
-//! 00500:014FF - Local APIC registers.
-//! 01500:015FF - CCS basic setup temporary memory.
-//! 01600:01FFF - free
+//! 01000:01FFF - Local APIC registers.
 //! 02000:02FFF - Paging. Page Table Level 1.
 //! 03000:03FFF - Paging. Page Table Level 2.
 //! 04000:04FFF - Paging. Page Table Level 3.
@@ -18,14 +17,14 @@
 
 /// Local APIC base registers address. They are moved from their default
 /// location here. Note that the registers are 4 KiB in size.
-pub const APIC_BASE_ADDRESS: u64 = 0x00500;
+pub const APIC_BASE_ADDRESS: u64 = 0x01000;
 
 /// The memory used to store information about basic kernel CCS objects and
 /// services when kernel starts up.
-pub const CCS_BASIC_SETUP_ADDRESS: usize = 0x01500;
+pub const CCS_BASIC_SETUP_ADDRESS: usize = 0x00100;
 
 /// The bound of the memory that can be used by CCS basic setup data.
-pub const CCS_BASIC_SETUP_ADDRESS_END: usize = 0x015FF;
+pub const CCS_BASIC_SETUP_ADDRESS_END: usize = 0x003FF;
 
 pub const PAGING_P1: usize = 0x2000;
 pub const PAGING_P2: usize = 0x3000;
