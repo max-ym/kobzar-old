d7348411f36605fc8df47e76f99c4d5bb623ad0e
Maxym Naumchyk <max.naumch@gmail.com>
2017-06-04 12:47:51 +0300

    Added docs

--- src/mem/arch/x86_64/paging.rs
+++ src/mem/arch/x86_64/paging.rs
@@ -1,6 +1,7 @@
 use arch::paging::*;
 use mem::map::{PAGING_P4, PAGING_P3, PAGING_P2, PAGING_P1};
 
+/// Get reference to P1 page struct of the kernel.
 fn p1() -> &'static mut P1 {
     unsafe {
         let a: *mut P1 = ::core::mem::transmute(PAGING_P1);
@@ -8,6 +9,7 @@ fn p1() -> &'static mut P1 {
     }
 }
 
+/// Get reference to P2 page struct of the kernel.
 fn p2() -> &'static mut P2 {
     unsafe {
         let a: *mut P2 = ::core::mem::transmute(PAGING_P2);
@@ -15,6 +17,7 @@ fn p2() -> &'static mut P2 {
     }
 }
 
+/// Get reference to P3 page struct of the kernel.
 fn p3() -> &'static mut P3 {
     unsafe {
         let a: *mut P3 = ::core::mem::transmute(PAGING_P3);
@@ -22,6 +25,7 @@ fn p3() -> &'static mut P3 {
     }
 }
 
+/// Get reference to P4 page struct of the kernel.
 fn p4() -> &'static mut P4 {
     unsafe {
         let a: *mut P4 = ::core::mem::transmute(PAGING_P4);
@@ -29,7 +33,9 @@ fn p4() -> &'static mut P4 {
     }
 }
 
-/// Initialize and load kernel paging table.
+/// Initialize and load kernel paging table. Creates regions for
+/// normal data and code. Also, disables cache for regions with
+/// mapped I/O devices.
 pub fn setup() {
     use arch::tentr::*;
 
