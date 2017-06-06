b70fb4e784b53f7370f9c2a55efbeeafd65ed2e3
Maxym Naumchyk <max.naumch@gmail.com>
2017-06-06 22:34:13 +0300

    Increased stack size

--- src/mem/arch/x86_64/map.rs
+++ src/mem/arch/x86_64/map.rs
@@ -1,17 +1,17 @@
 //! Memory map after launching the kernel.
-//! 00000:003FF - Stack.
+//! 00000:003FF - free
 //! 00400:004FF - BIOS Data Area.
 //! 00500:7FFFF - free
 //! 7C000:7CFFF - Initial paging. Page Table Level 1.
 //! 7D000:7DFFF - Initial paging. Page Table Level 2.
 //! 7E000:7EFFF - Initial paging. Page Table Level 3.
-//! 7F000:7FFFF - free
+//! 7F000:7FFFF - Stack.
 //! 80000:9FBFF - free but may not all be used.
 //! 9FC00:9FFFF - typical location for EBDA. But no guarantees.
 //! A0000:FFFFF - Video + ROM. Must not be cached, write through.
 //!
 //! Memory map of the kernel in selected region:
-//! 00000:003FF - Stack.
+//! 00000:003FF - free
 //! 00400:004FF - BIOS Data Area.
 //! 00500:00FFF - CCS basic setup temporary memory.
 //! 01000:01FFF - Local APIC registers.
@@ -23,7 +23,7 @@
 //! 7C000:7CFFF - Initial paging. Page Table Level 1.
 //! 7D000:7DFFF - Initial paging. Page Table Level 2.
 //! 7E000:7EFFF - Initial paging. Page Table Level 3.
-//! 7F000:7FFFF - free
+//! 7F000:7FFFF - Stack.
 //!
 //! Note that as soon as second paging tables are enabled,
 //! memory region of 7C000:7EFFF gets free.
