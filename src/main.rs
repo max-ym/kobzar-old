eeb70bd9d5c7778d12e28cd1914c6899f160035f
Maxym Naumchyk <max.naumch@gmail.com>
2017-06-06 18:26:09 +0300

    New fn 'memset'

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,93 @@ fn setup_interrupts() {
     ::early::setup_interrupts();
 }
 
+#[cfg(target_arch = "x86_64")]
+extern "C" {
+    fn memset_avx(dest: *mut u8, c: u64, n: usize);
+}
 
+#[no_mangle]
+#[cfg(target_arch = "x86_64")]
+pub unsafe extern "C" fn memset(dest: *mut u8, c: u8, n: usize) -> *mut u8 {
+    let mut dest    = dest;
+    let     res     = dest.clone();
+    let mut n       = n as usize;
+
+    if n == 0 {
+        return res;
+    }
+
+    // Get 8-byte pattern.
+    let c = {
+        let c = c as u64;
+        let a = c << 8;
+        let c = c | a;
+
+        let a = c << 16;
+        let c = c | a;
+
+        let a = c << 32;
+        c | a
+    };
+
+    unsafe fn fill1(dest: &mut *mut u8, c: u64, n: &mut usize) {
+        **dest = c as u8;
+        *dest = (*dest).offset(1);
+        *n = *n - 1;
+    };
+
+    unsafe fn align2(dest: &mut *mut u8, c: u64, n: &mut usize) {
+        let dest_addr = *dest as usize;
+        if dest_addr % 2 != 0 {
+            // Align to 2-byte boundary.
+            fill1(dest, c, n);
+        }
+    };
+
+    unsafe fn fill2(mut count: usize, dest: &mut *mut u8, c: u64, n: &mut usize) {
+        *n = *n - count * 2;
+        while count > 0 {
+            let d = *dest as *mut u16;
+            *d = c as u16;
+            *dest = (*dest).offset(2);
+            count = count - 1;
+        }
+    };
+
+    unsafe fn fill8(mut count: usize, dest: &mut *mut u8, c: u64, n: &mut usize) {
+        *n = *n - count * 8;
+        while count > 0 {
+            let d = *dest as *mut u64;
+            *d = c;
+            *dest = (*dest).offset(8);
+            count = count - 1;
+        }
+    };
+
+    if n == 2 {
+        fill2(1, &mut dest, c, &mut n);
+        return res;
+    }
+    align2(&mut dest, c, &mut n);
+    fill2((n %  8) / 2, &mut dest, c, &mut n);
+    fill8((n % 32) / 8, &mut dest, c, &mut n);
+    fill2((n % 32) / 2, &mut dest, c, &mut n);
+    align2(&mut dest, c, &mut n);
+
+    if n > 32 {
+        // TODO support processors with no AVX.
+        memset_avx(dest, c, n / 32);
+    }
+
+    // Fill the end which gone out of 32-byte boundary.
+    fill8(n / 8, &mut dest, c, &mut n);
+    fill2(n / 2, &mut dest, c, &mut n);
+    if n == 1 {
+        fill1(&mut dest, c, &mut n);
+    }
+
+    res
+}
 
 #[lang = "eh_personality"]
 #[no_mangle]
