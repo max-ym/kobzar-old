56c6f4593654c45e20132c8f61ea204c253989e2
Maxym Naumchyk <max.naumch@gmail.com>
2017-07-20 00:28:20 +0300

    Started reimplementing. Removed CodeSegmentDescriptor which does not exist in x86_64 and made all descriptors 16-byte

--- src/arch/x86_64/dt/gdt/desc.rs
+++ src/arch/x86_64/dt/gdt/desc.rs
@@ -3,27 +3,17 @@ use super::Dpl;
 /// Struct to represent general GDT descriptor type.
 #[repr(packed)]
 pub struct GdtDescriptor {
-    data    : u64,
+    data    : [u64; 2]
 }
 
 use super::{Entry, EntryVariant};
 impl Entry for GdtDescriptor {
 }
 
-/// The Code Segment Descriptor.
-#[repr(packed)]
-pub struct CodeSegmentDescriptor {
-    a   : u32,
-    _b  : u32, // This one is unused.
-}
-
-impl Entry for CodeSegmentDescriptor {
-}
-
 /// The first descriptor in GDT is null.
 #[repr(packed)]
 pub struct NullDescriptor {
-    null    : u64
+    null    : [u64; 2]
 }
 
 impl Entry for NullDescriptor {
@@ -33,60 +23,15 @@ impl Default for NullDescriptor {
 
     fn default() -> Self {
         NullDescriptor {
-            null: 0
+            null: [0, 0]
         }
-    }
-}
-
-/// Code Segment Descriptor flag list.
-#[repr(u32)]
-enum CsdFlag {
-
-    Accessed        = 1 << 8,
-    Readable        = 1 << 9,
-    Conforming      = 1 << 10,
-
-    Present         = 1 << 15,
-
-    Available       = 1 << 20,
-    LongMode        = 1 << 21,
-    DefaultBit      = 1 << 22,
-    Granularity     = 1 << 23,
-
-    Dpl             = (1 << 13) | (1 << 14),
-}
-
-/// Use this macro to create a getter for CSD bit.
-macro_rules! getter {
-    ($n: tt, $x:tt) => {
-        #[inline(always)]
-        pub fn $n(&self) -> bool {
-            self.a & (CsdFlag::$x as u32) != 0
-        }
-    };
-}
-
-impl CodeSegmentDescriptor {
-
-    getter!(accessed        , Accessed);
-    getter!(readable        , Readable);
-    getter!(conforming      , Conforming);
-    getter!(present         , Present);
-    getter!(available       , Available);
-    getter!(long_mode       , LongMode);
-    getter!(default         , DefaultBit);
-    getter!(granularity     , Granularity);
-
-    pub fn dpl(&self) -> Dpl {
-        let val = self.a & (CsdFlag::Dpl as u32) >> 13;
-        unsafe { ::core::mem::transmute(val) }
     }
 }
 
 impl EntryVariant<NullDescriptor> for GdtDescriptor {
 
     fn try_variant_ref(&self) -> Option<&NullDescriptor> {
-        if self.data == 0 {
+        if self.data[0] == 0 && self.data[1] == 0 {
             unsafe { Some(::core::mem::transmute(self)) }
         } else {
             None
@@ -94,7 +39,7 @@ impl EntryVariant<NullDescriptor> for GdtDescriptor {
     }
 
     fn try_variant_mut(&mut self) -> Option<&mut NullDescriptor> {
-        if self.data == 0 {
+        if self.data[0] == 0 && self.data[1] == 0 {
             unsafe { Some(::core::mem::transmute(self)) }
         } else {
             None
