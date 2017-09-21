6568b1cef202ad354cc48a8eda53962019b1e8b7
Maxym Naumchyk <max.naumch@gmail.com>
2017-09-21 23:40:29 +0300

    New traits ProcessHandle and SchedulerCore

--- src/ccs/sched.rs
+++ src/ccs/sched.rs
@@ -2,9 +2,6 @@
 /// Process is a runnable instance of some service with allocated
 /// metadata and working environment.
 pub trait Process {
-
-    /// Process current state.
-    pub state(&self) -> ProcessState;
 }
 
 /// All process states.
@@ -28,4 +25,50 @@ pub enum ProcessState {
     /// Process is waiting for external signal to wake him up for limited time.
     /// If time goes out, timer will wake this process up.
     TimedWait,
+}
+
+/// Handle of process for specific sheduler implementation.
+trait ProcessHandle {
+
+    /// Process that trait handles.
+    type P : Process;
+
+    /// Process current state.
+    fn state(&self) -> ProcessState;
+
+    /// Store this process in paused list if possible.
+    /// If process is terminated, finished etc. it cannot be stored in the
+    /// list.
+    fn save_as_paused(&mut self) -> Option<()>;
+
+    /// Store this process in vacant process list if possible.
+    /// If process is terminated, finished etc. it cannot be stored in the
+    /// list.
+    fn save_as_vacant(&mut self) -> Option<()>;
+
+    /// Check if process can be stored either in paused or vacant process
+    /// list.
+    fn is_saveable(&self) -> bool {
+        use self::ProcessState::*;
+        match self.state() {
+            Running         => true,
+            Pause           => true,
+            SchedulerPause  => true,
+
+            End             => false,
+            Wait            => false,
+            TimedWait       => false,
+        }
+    }
+}
+
+/// The core mechanisms of scheduler which are not visible outside this
+/// module.
+trait SchedulerCore {
+
+    /// Process handle of this scheduler.
+    type P : ProcessHandle;
+
+    /// Get next process to run and remove it from vacant process list.
+    fn next_vacant_process(&mut self) -> Self::P;
 }
