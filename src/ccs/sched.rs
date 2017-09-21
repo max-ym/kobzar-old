d6128e3f98b4e49eda88372ca5189b2dd481daa5
Maxym Naumchyk <max.naumch@gmail.com>
2017-09-21 23:04:25 +0300

    New enum ProcessState

--- src/ccs/sched.rs
+++ src/ccs/sched.rs
@@ -3,3 +3,26 @@
 /// metadata and working environment.
 pub trait Process {
 }
+
+/// All process states.
+pub enum ProcessState {
+
+    /// Process is currently running.
+    Running,
+
+    /// Process has ended execution or was terminated.
+    End,
+
+    /// Process was paused by something.
+    Pause,
+
+    /// Scheduler removed processor time from this process.
+    SchedulerPause,
+
+    /// Process is waiting for external signal to wake him up.
+    Wait,
+
+    /// Process is waiting for external signal to wake him up for limited time.
+    /// If time goes out, timer will wake this process up.
+    TimedWait,
+}
