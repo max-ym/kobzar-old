86c11257bc7b14fa4db5b68c69c31e6bb30fa36c
Max Naumch <max.naumch@gmail.com>
2017-09-22 21:50:43 +0300

    Started impl for LinkedList + minor changes to module

--- src/collections.rs
+++ src/collections.rs
@@ -1,11 +1,42 @@
 
 /// Linked List implementation.
 pub struct LinkedList<T> {
-    top     : *mut LinkedListNode<T>
+    top     : *mut LinkedListNode<T>,
+    bot     : *mut LinkedListNode<T>,
 }
 
 /// Node of linked list.
-struct LinkedListNode<T> {
-    pub next    : *mut LinkedListNode<T>,
-    pub data    : T,
+pub struct LinkedListNode<T> {
+    next    : *mut LinkedListNode<T>,
+    data    : T,
+}
+
+/// Memory Allocator for specified type.
+pub trait MemoryAllocator<T> {
+
+    fn next(&mut self, t: T) -> *mut T;
+}
+
+impl<T> LinkedList<T> {
+
+    /// Create empty linked list.
+    pub fn new() -> Self {
+        LinkedList {
+            top : ::core::ptr::null_mut::<LinkedListNode<T>>(),
+            bot : ::core::ptr::null_mut::<LinkedListNode<T>>(),
+        }
+    }
+
+    pub fn push_front<MA : MemoryAllocator<LinkedListNode<T>>>
+            (&mut self, mem: &mut MA, t: T) {
+        let ptr = mem.next(LinkedListNode {
+            next: ::core::ptr::null_mut::<LinkedListNode<T>>(),
+            data: t,
+        });
+
+        unsafe {
+            (*self.bot).next = ptr;
+            self.bot = ptr;
+        }
+    }
 }
