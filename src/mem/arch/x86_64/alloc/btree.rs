81f3864a5d5a378629feb241bb07c3293d07fa2a
Maxym Naumchyk <max.naumch@gmail.com>
2017-09-25 00:14:27 +0300

    New mod btree

---
+++ src/mem/arch/x86_64/alloc/btree.rs
@@ -0,0 +1,11 @@
+/// B-Tree of 2MiB pages.
+struct PageBTree {
+}
+
+struct BTreeNode {
+}
+
+#[repr(packed)]
+struct BTreeLeaf {
+
+}
--- src/mem/arch/x86_64/alloc/mod.rs
+++ src/mem/arch/x86_64/alloc/mod.rs
@@ -5,3 +5,6 @@ use self::p2m::Page2m;
 
 /// Allocator for 4KiB pages.
 pub mod p4k;
+
+/// B-Tree structure that stores all usable 2MiB pages and their status.
+pub mod btree;
