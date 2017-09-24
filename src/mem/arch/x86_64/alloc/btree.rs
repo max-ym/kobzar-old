82ac27590002affd56ed11dd440b36220c334795
Maxym Naumchyk <max.naumch@gmail.com>
2017-09-25 00:43:58 +0300

    Defined BTreeLeaf

--- src/mem/arch/x86_64/alloc/btree.rs
+++ src/mem/arch/x86_64/alloc/btree.rs
@@ -1,3 +1,5 @@
+use super::p2m::Page2mStatus;
+
 /// B-Tree of 2MiB pages.
 pub struct PageBTree {
 }
@@ -5,7 +7,9 @@ pub struct PageBTree {
 pub struct BTreeNode {
 }
 
+/// Leaf of B-tree. It was calculated that optimal leaf element count is 4.
+/// So each leaf stores 4 pointers to page status structures in the heap.
 #[repr(packed)]
 pub struct BTreeLeaf {
-
+    arr     : [*mut Page2mStatus; 4]
 }
