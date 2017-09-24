0a46aee468d03be5a07517225ec093fb63b7e7be
Maxym Naumchyk <max.naumch@gmail.com>
2017-09-25 00:51:27 +0300

    New fn get_page in BTreeLeaf

--- src/mem/arch/x86_64/alloc/btree.rs
+++ src/mem/arch/x86_64/alloc/btree.rs
@@ -1,3 +1,4 @@
+use super::p2m::Page2m;
 use super::p2m::Page2mStatus;
 
 /// B-Tree of 2MiB pages.
@@ -12,4 +13,26 @@ pub struct BTreeNode {
 #[repr(packed)]
 pub struct BTreeLeaf {
     arr     : [*mut Page2mStatus; 4]
+}
+
+impl BTreeLeaf {
+
+    /// Get given page status structure pointer if it is stored in this
+    /// leaf.
+    pub fn get_page(&self, p: &Page2m) -> Option<*mut Page2mStatus> {
+        let index = p.addr() / (1024 * 2048) % 4;
+        let ptr = self.arr[index as usize];
+
+        if ptr as usize == 0 {
+            None
+        } else {
+            unsafe {
+                if (*ptr).page_address() == p.addr() {
+                    Some(ptr)
+                } else {
+                    None
+                }
+            }
+        }
+    }
 }
