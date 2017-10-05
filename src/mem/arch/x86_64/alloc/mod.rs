79bd2507ae4b602fa38766733ad98ae42ce967c0
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-05 23:25:37 +0300

    Reformat

--- src/mem/arch/x86_64/alloc/mod.rs
+++ src/mem/arch/x86_64/alloc/mod.rs
@@ -1,23 +1,23 @@
 /// Allocator for 2MiB pages.
 pub mod p2m;
 
-pub use self::p2m::Page2m;
-pub use self::p2m::Stack as Stack2m;
-
 /// Allocator for 4KiB pages.
 pub mod p4k;
-
-pub use self::p4k::Page4k;
 
 /// Page Status Object module.
 pub mod pso;
 
+/// Page Map Heap module.
+pub mod map_heap;
+
+pub use self::p2m::Page2m;
+pub use self::p2m::Stack as Stack2m;
+
+pub use self::p4k::Page4k;
+
 pub use self::pso::PageStatus;
 pub use self::pso::PsArray;
 pub use self::pso::PsaArray;
-
-/// Page Map Heap module.
-pub mod map_heap;
 
 pub use self::map_heap::RelativeAddress;
 pub use self::map_heap::HeapEntry as Heap4kEntry;
