e2332d7802d91421a505a872e30ad9aaba68a768
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-05 21:04:08 +0300

    New struct Bitmap and impl Default

--- src/mem/arch/x86_64/alloc/map_heap.rs
+++ src/mem/arch/x86_64/alloc/map_heap.rs
@@ -1,1 +1,18 @@
+/// Bitmap of allocated/free 4KiB pages.
+#[repr(packed)]
+pub struct Bitmap {
+    /// Array of bytes of the bitmap. Bytes count is:
+    ///
+    /// 2048 - 2MiB page size; divided by 4 - 4KiB page size that this bigger
+    /// page was split into; divided by 8 - bits count in one byte.
+    pub arr     : [u8; 2048 / 4 / 8],
+}
 
+impl Default for Bitmap {
+
+    fn default() -> Self {
+        Bitmap {
+            arr     : [0; 64]
+        }
+    }
+}
