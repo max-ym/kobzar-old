539f3900a04032108eebf9b13577346d21592dce
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-05 11:08:51 +0300

    Impl Range

--- src/mem/arch/x86_64/alloc/pso.rs
+++ src/mem/arch/x86_64/alloc/pso.rs
@@ -18,3 +18,28 @@ pub struct PSOArray {
     range   : Range,
     arr     : *mut PageStatus,
 }
+
+impl Range {
+
+    /// Create new range.
+    pub fn new(top: u32, bottom: u32) -> Self {
+        Range {
+            hi  : top,
+            low : bottom,
+        }
+    }
+
+    /// How many entries this range contains.
+    pub fn length(&self) -> u64 {
+        (self.hi - self.low) / 0x200000
+    }
+
+    /// Get index of a page status entry for this page address.
+    ///
+    /// # Safety
+    /// It is expected that given absolute address of a page is
+    /// within this range.
+    pub unsafe fn abs_to_index(&self, absolute: u64) -> u64 {
+        (absolute - self.low) / 0x200000
+    }
+}
