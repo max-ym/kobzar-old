be93f188bc1c06969ee0a8dd2c9e75d993e4cfb3
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-06 00:42:15 +0300

    Docs edit

--- src/mem/arch/x86_64/alloc/mod.rs
+++ src/mem/arch/x86_64/alloc/mod.rs
@@ -4,10 +4,15 @@ pub mod p2m;
 /// Structures related to 4KiB pages.
 pub mod p4k;
 
-/// Page Status Object module.
+/// Page Status Object module. Page Status holds information that is used
+/// in page allocator. For example, it holds a counter of how many page
+/// table entries use this page. This allows allocator to know when page
+/// gets free from users and whether it can be safely allocated for
+/// new processes.
 pub mod pso;
 
-/// Page Map Heap module.
+/// Page Map Heap module. Heap stores Page Status entries for each 4KiB page
+/// that were created by dividing 2MiB pages.
 pub mod map_heap;
 
 pub use self::p2m::Page2m;
