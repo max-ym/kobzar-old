01919422b487254c49389c405dd908318d0bcea7
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-14 02:23:19 +0300

    Initial fns for Alloc struct (not all implemented)

--- src/mem/arch/x86_64/alloc/ctrl.rs
+++ src/mem/arch/x86_64/alloc/ctrl.rs
@@ -1,3 +1,6 @@
+use super::Page4k;
+use super::Page2m;
+
 type Result<T> = ::core::result::Result<T, AllocError>;
 
 /// Pages allocator.
@@ -12,4 +15,44 @@ pub enum AllocError {
 
 impl Alloc {
 
+    pub fn alloc4k(&mut self) -> Result<Page4k> {
+        unimplemented!()
+    }
+
+    pub fn alloc2m(&mut self) -> Result<Page2m> {
+        unimplemented!()
+    }
+
+    pub fn release4k(&mut self, page: Page4k) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn release2m(&mut self, page: Page2m) -> Result<()> {
+        unimplemented!()
+    }
+
+    /// Amount of free 2MiB pages.
+    pub fn free2m_size(&self) -> usize {
+        unimplemented!()
+    }
+
+    /// Amount of free memory in bytes that are covered by 2MiB pages.
+    pub fn free2m_bytes(&self) -> usize {
+        self.free2m_size() * 2048 * 1024
+    }
+
+    /// Amount of free 4KiB pages.
+    pub fn free4k_size(&self) -> usize {
+        unimplemented!()
+    }
+
+    /// Amount of free memory in bytes that are covered by 4KiB pages.
+    pub fn free4k_bytes(&self) -> usize {
+        self.free4k_size() * 4096
+    }
+
+    /// Amount of free memory.
+    pub fn free_memory_size(&self) -> usize {
+        self.free2m_bytes() + self.free4k_bytes()
+    }
 }
