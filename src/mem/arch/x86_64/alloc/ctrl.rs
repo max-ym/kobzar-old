4d65663b1b5e210a6ffb650e31bebdbb136d2c4b
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-14 02:12:13 +0300

    Initial ctrl mod

--- src/mem/arch/x86_64/alloc/ctrl.rs
+++ src/mem/arch/x86_64/alloc/ctrl.rs
@@ -1,1 +1,15 @@
+type Result<T> = ::core::result::Result<T, AllocError>;
 
+/// Pages allocator.
+pub struct Alloc {
+    // TODO
+}
+
+/// Errors that allocator can return when some action cannot be
+/// performed for any reason.
+pub enum AllocError {
+}
+
+impl Alloc {
+
+}
