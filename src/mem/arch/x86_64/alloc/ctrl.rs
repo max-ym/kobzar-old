67fe6d4c5c4cded6abbf35b925f437c5f6f8a1a0
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-15 02:44:23 +0300

    Improved docs

--- src/mem/arch/x86_64/alloc/ctrl.rs
+++ src/mem/arch/x86_64/alloc/ctrl.rs
@@ -13,7 +13,7 @@ pub struct Alloc {
     /// Stack that contains all 2MiB pages that are free for allocation.
     stk2    : Stack2m,
 
-    /// Array that contains page ranges that are controlled by memory
+    /// Array that contains 2MiB page ranges that are controlled by memory
     /// controller. Each range itself is a fixed size array with page status
     /// objects of individual pages.
     psa     : PsaArray,
