25efed1f2a79e7427f739822e9ef103249991f88
Maxym Naumchyk <max.naumch@gmail.com>
2017-11-17 17:20:06 +0200

    New allocator traits in mem module

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -1,2 +1,26 @@
 mod arch;
 pub use self::arch::*;
+
+use Address;
+
+/// Minimal memory allocator.
+pub trait Allocator {
+
+    /// Allocate next range of bytes.
+    fn alloc(&mut self, size: usize) -> Address;
+}
+
+/// Allocator for particular data type.
+pub trait TypedAllocator : Allocator {
+
+    /// The type of a value to be allocated.
+    type T;
+
+    /// Allocate array of value type (uninitialized).
+    fn next(&mut self, count: usize) -> *mut Self::T {
+        use ::core::mem::size_of;
+
+        let addr: usize = self.alloc(size_of::<Self::T>() * count).into();
+        addr as *const Self::T as _
+    }
+}
