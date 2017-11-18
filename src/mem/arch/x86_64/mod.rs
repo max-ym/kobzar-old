d41b15059d538fed857bbbb1670f15b31fdb1660
Maxym Naumchyk <max.naumch@gmail.com>
2017-11-18 22:21:05 +0200

    New kernel allocator

--- src/mem/arch/x86_64/mod.rs
+++ src/mem/arch/x86_64/mod.rs
@@ -9,3 +9,16 @@ pub mod paging;
 
 /// Global Descriptor Table of the kernel.
 pub mod gdt;
+
+use super::TopLimitedAllocator;
+
+/// Main kernel memory allocator.
+/// Is allowed to be used only when kernel paging was re-initialized.
+static MAIN_ALLOC: TopLimitedAllocator = new_main_alloc();
+
+const fn new_main_alloc() -> TopLimitedAllocator {
+    use super::super::Address;
+
+    TopLimitedAllocator::new(Address::new_from_usize(map::MEMALLOC_START),
+            Address::new_from_usize(map::MEMALLOC_END))
+}
