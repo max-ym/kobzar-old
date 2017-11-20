f75cc393fb00281f6a786da54a66abe26b562559
Maxym Naumchyk <max.naumch@gmail.com>
2017-11-20 13:48:25 +0200

    New trait SchedProcesses + HandleSet renamed to ProcessSet

--- src/ccs/sched/list.rs
+++ src/ccs/sched/list.rs
@@ -6,9 +6,9 @@ use super::ProcessHandle;
 pub enum ProcessAllocErr {
 }
 
-/// Process Handle Set. This set stores all process handles. Different
+/// Process handle set. This set stores all process handles. Different
 /// scheduler process lists point to process handles in this set.
-pub trait HandleSet {
+pub trait ProcessSet {
 
     /// Process Handle type that is stored in this Process Handle Set.
     type P : ProcessHandle;
@@ -70,4 +70,50 @@ pub trait ProcessQueue {
 
     /// Append given value to the queue.
     fn append(&mut self, p: &mut Self::P);
+}
+
+/// Heap of queues, lists and sets of processes used in scheduler architecture.
+pub trait SchedProcesses {
+
+    /// Paused tasks handle set.
+    type PT : ProcessSet;
+
+    /// Queue of tasks.
+    type TQ : ProcessQueue;
+
+    /// Paused processes.
+    type PP : ProcessSet;
+
+    /// Vacant processes that wait their processor time.
+    type VP : ProcessQueue;
+
+    /// Paused tasks.
+    fn paused_tasks(&self) -> &Self::PT;
+
+    /// Paused tasks.
+    fn paused_tasks_mut(&mut self) -> &mut Self::PT {
+        unsafe { &mut *(self.paused_tasks() as *const Self::PT as *mut _) }
+    }
+
+    /// Tasks in queue that are waiting for processor time.
+    fn task_queue(&self) -> &Self::TQ;
+
+    fn task_queue_mut(&mut self) -> &mut Self::TQ {
+        unsafe { &mut *(self.task_queue() as *const Self::TQ as *mut _) }
+    }
+
+    /// Processes that were paused by external thing. They will continue
+    /// executing when they get unpaused by someone.
+    fn paused_procs(&self) -> &Self::PP;
+
+    fn paused_procs_mut(&mut self) -> &mut Self::PP {
+        unsafe { &mut *(self.paused_procs() as *const Self::PP as *mut _) }
+    }
+
+    /// Processes that wait their processor time.
+    fn vacant_procs(&self) -> &Self::VP;
+
+    fn vacant_procs_mut(&mut self) -> &mut Self::VP {
+        unsafe { &mut *(self.vacant_procs() as *const Self::VP as *mut _) }
+    }
 }
