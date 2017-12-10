97bed2c1f0c4baaedf7be07fb73c35a09f2bbd9e
Maxym Naumchyk <max.naumch@gmail.com>
2017-12-11 00:12:29 +0200

    Started impl alloc mod

--- src/ccs/sched/arch/x86_64/alloc.rs
+++ src/ccs/sched/arch/x86_64/alloc.rs
@@ -1,1 +1,29 @@
- 
+use mem;
+
+/// Process Handle Allocator.
+pub struct PHandleAlloc {
+
+}
+
+/// A single frame of allocator that covers 64 entries
+/// process handles.
+pub struct PhAllocFrame {
+    bitmap  : u64,
+    mem     : [*mut PHandleAlloc; 64],
+}
+
+impl PHandleAlloc {
+
+    /// Find frame with free cell.
+    fn frame_with_free(&self) -> &PhAllocFrame {
+        unimplemented!()
+    }
+
+    /// Find frame with free cell.
+    fn frame_with_free_mut(&mut self) -> &mut PHandleAlloc {
+        let ptr = self.frame_with_free() as *const PhAllocFrame as *mut _;
+        unsafe { &mut *ptr }
+    }
+
+
+}
