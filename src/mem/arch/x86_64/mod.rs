d2d893c2f4ad14ecc291e5ce8e4c2a8de28e40f8
Maxym Naumchyk <max.naumch@gmail.com>
2018-01-07 06:01:05 +0200

    New memory manager module mgr

--- src/mem/arch/x86_64/mod.rs
+++ src/mem/arch/x86_64/mod.rs
@@ -2,3 +2,7 @@ use super::*;
 
 // The memory map of kernel image. Module doc is placed in 'map.rs' sources.
 pub mod map;
+
+/// Memory manager. Implements a system object that provides services of
+/// memory allocation.
+pub mod mgr;
