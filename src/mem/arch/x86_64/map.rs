0db876a6652a724167a9ac80912018df2d60741a
Maxym Naumchyk <max.naumch@gmail.com>
2018-01-11 23:32:38 +0200

    Implemented map.rs with addr! macro

--- src/mem/arch/x86_64/map.rs
+++ src/mem/arch/x86_64/map.rs
@@ -13,7 +13,7 @@
 //! Memory map of the kernel in selected region:
 //! 00000:003FF - free
 //! 00400:004FF - BIOS Data Area.
-//! 00500:00FFF - CCS basic setup temporary memory.
+//! 00500:00FFF - free
 //! 01000:01FFF - Local APIC registers.
 //! 02000:02FFF - Paging. Page Table Level 1.
 //! 03000:03FFF - Paging. Page Table Level 2.
@@ -48,39 +48,42 @@
 
 use super::Address;
 
-// TODO consider using macro for creating constants.
+macro_rules! addr {
+    ($name:ident, $addr:expr, $docs:expr) => {
+        #[doc=$docs]
+        pub const $name: Address = Address::new_from_usize($addr as usize);
+    };
 
-/// Local APIC base registers address. They are moved from their default
-/// location here. Note that the registers are 4 KiB in size.
-pub const APIC_BASE: Address = Address::new_from_usize(0x01000usize);
+    ($name:ident, $addr:expr) => {
+        addr!($name, $addr, "");
+    };
+}
 
-/// The memory used to store information about basic kernel CCS objects and
-/// services when kernel starts up.
-pub const CCS_BASIC_SETUP: Address = Address::new_from_usize(0x00500usize);
+addr!(APIC_BASE, 0x1000, "
+    Local APIC base registers address. They are moved from their default
+    location here. Note that the registers are 4 KiB in size.
+");
 
-/// The bound of the memory that can be used by CCS basic setup data.
-pub const CCS_BASIC_SETUP_END: Address = Address::new_from_usize(0x00FFFusize);
+addr!(PAGING_P1, 0x2000, "
+    Address of the level 1 paging table of the kernel.
+");
 
-/// Address of the level 1 paging table of the kernel.
-pub const PAGING_P1: Address = Address::new_from_usize(0x2000usize);
+addr!(PAGING_P2, 0x3000, "
+    Address of the level 2 paging table of the kernel.
+");
 
-/// Address of the level 2 paging table of the kernel.
-pub const PAGING_P2: Address = Address::new_from_usize(0x3000usize);
+addr!(PAGING_P3, 0x4000, "
+    Address of the level 3 paging table of the kernel.
+");
 
-/// Address of the level 3 paging table of the kernel.
-pub const PAGING_P3: Address = Address::new_from_usize(0x4000usize);
+addr!(PAGING_P4, 0x5000, "
+    Address of the level 4 paging table of the kernel.
+");
 
-/// Address of the level 4 paging table of the kernel.
-pub const PAGING_P4: Address = Address::new_from_usize(0x5000usize);
+addr!(IDT, 0x6000, "
+    Address of Interrupt Descriptor Table.
+");
 
-/// Address of Interrupt Descriptor Table.
-pub const IDT: Address = Address::new_from_usize(0x6000usize);
-
-/// Address of Global Descriptor Table.
-pub const GDT: Address = Address::new_from_usize(0x7000usize);
-
-/// Start of kernel memory allocator.
-pub const MEMALLOC_START: Address = Address::new_from_usize(0x7C000usize);
-
-/// End of kernel memory allocator (excluding byte at this address).
-pub const MEMALLOC_END: Address = Address::new_from_usize(0x7F000usize);
+addr!(GDT, 0x7000, "
+    Address of Global Descriptor Table.
+");
