547edfe2551701c5e5dd02d127a987a23bb3aa8e
Maxym Naumchyk <max.naumch@gmail.com>
2018-01-14 06:32:11 +0200

    Added many iterators

--- src/mem/arch/x86_64/mgr/page.rs
+++ src/mem/arch/x86_64/mgr/page.rs
@@ -1,11 +1,29 @@
+use core::ops::Range;
 use mem::Address;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
 /// 2MiB page wrap.
 pub struct Page2m {
 
     /// Address of a page.
     addr    : Address
+}
+
+/// Infinite iterator over 2 MiB pages.
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+pub struct Page2mIter {
+
+    page    : Page2m,
+}
+
+/// Iterator over 2 MiB page range.
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+pub struct Page2mRangeIter {
+
+    start   : Page2m,
+    end     : Page2m,
+
+    cur     : Page2m,
 }
 
 impl Page2m {
@@ -33,5 +51,81 @@ impl Page2m {
     /// Address of a 2 MiB memory covered by this page wrapper.
     pub fn addr(&self) -> Address {
         self.addr
+    }
+}
+
+impl Iterator for Page2mIter {
+
+    type Item = Page2m;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let addr = self.page.addr + Page2m::SIZE;
+        if addr < self.page.addr {
+            None // Overflow
+        } else {
+            Some(Page2m { addr })
+        }
+    }
+}
+
+impl DoubleEndedIterator for Page2mIter {
+
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.page.addr < Page2m::SIZE {
+            None
+        } else {
+            let addr = self.page.addr - Page2m::SIZE;
+            Some(Page2m { addr })
+        }
+    }
+}
+
+impl Iterator for Page2mRangeIter {
+
+    type Item = Page2m;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur == self.end {
+            return None;
+        }
+
+        let addr = self.cur.addr + Page2m::SIZE;
+        self.cur.addr += Page2m::SIZE;
+        Some(Page2m { addr })
+    }
+}
+
+impl DoubleEndedIterator for Page2mRangeIter {
+
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.cur == self.start {
+            return None;
+        }
+
+        let addr = self.cur.addr - Page2m::SIZE;
+        self.cur.addr -= Page2m::SIZE;
+        Some(Page2m { addr })
+    }
+}
+
+impl ExactSizeIterator for Page2mRangeIter {
+
+    fn len(&self) -> usize {
+        let sub = self.end.addr - self.cur.addr;
+        let sub: usize = sub.into();
+        sub / Page2m::SIZE
+    }
+}
+
+impl Page2mRangeIter {
+
+    /// Create range iterator for given range.
+    pub fn for_range(range: &Range<Page2m>) -> Self {
+        Page2mRangeIter {
+            start   : range.start,
+            end     : range.end,
+
+            cur     : range.start,
+        }
     }
 }
