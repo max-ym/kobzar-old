878623961bc80fb5deb3a349a6754e0d3d10c968
Maxym Naumchyk <max.naumch@gmail.com>
2018-01-14 04:28:03 +0200

    Impl ArrayEntry

--- src/mem/arch/x86_64/mgr/tree.rs
+++ src/mem/arch/x86_64/mgr/tree.rs
@@ -42,6 +42,107 @@ struct DivData {
     counters: [ProcessCount; 512],
 }
 
+/// Error that occurs when 2 MiB page gets divided and fails.
+enum PageDivisionError {
+
+    /// Page already is divided and so this operation cannot be performed.
+    AlreadyDivided,
+
+    /// Page is allocated as one and so cannot be used for division until
+    /// everyone who uses the page releases it.
+    Allocated,
+}
+
+/// Error that occurs when 2 MiB page gets merged and fails.
+enum PageMergeError {
+
+    /// Cannot merge the page that is not divided in first place.
+    NotDivided,
+
+    /// Some page parts are still in use among the system. Cannot merge the
+    /// page having it's slices used.
+    Used,
+}
+
+impl ArrayEntry {
+
+    /// Whether the 2 MiB page that is covered by the entry was divided
+    /// into 512 4 KiB pages or remains as one 2 MiB.
+    pub fn is_divided(&self) -> bool {
+        self.is_divided
+    }
+
+    /// How many times the 2 MiB page was shared. None returns when page
+    /// was divided and cannot be possibly shared as 2 MiB.
+    pub fn share_count(&self) -> Option<ProcessCount> {
+        if self.is_divided() {
+            None
+        } else {
+            unsafe { Some(self.data.alloc_count) }
+        }
+    }
+
+    /// Information about page division. if covered page was not yet divided
+    /// then None will be returned.
+    pub fn div_data(&self) -> Option<&DivData> {
+        if self.is_divided() {
+            unsafe { Some(&*self.data.div_data) }
+        } else {
+            None
+        }
+    }
+
+    /// Information about page division. if covered page was not yet divided
+    /// then None will be returned.
+    pub fn div_data_mut(&mut self) -> Option<&mut DivData> {
+        if self.is_divided() {
+            unsafe { Some(&mut *self.data.div_data) }
+        } else {
+            None
+        }
+    }
+
+    /// Divide given 2 MiB page into 512 4 KiB pages. For Err details see
+    /// enum.
+    ///
+    /// # Safety
+    /// Caller must provide proper memory for storing DivData. Failing to
+    /// do so may cause data corruption and errors.
+    pub unsafe fn divide(&mut self, div_data_storage: *mut DivData)
+            -> Result<(), PageDivisionError> {
+        use self::PageDivisionError::*;
+
+        if self.is_divided() {
+            return Err(AlreadyDivided);
+        }
+        if self.share_count().unwrap() > 0 {
+            return Err(Allocated);
+        }
+
+        self.is_divided = true;
+        unsafe { self.data.div_data = div_data_storage; }
+        Ok(())
+    }
+
+    /// Merge divided 2 MiB page back into one. For Err details see
+    /// enum. On success returns the DivData memory pointer that was used
+    /// to store the division data and is no longer needed. Memory allocator
+    /// may free that memory or system may use it in other way now.
+    pub fn merge(&mut self) -> Result<*mut DivData, PageMergeError> {
+        use self::PageMergeError::*;
+
+        if !self.is_divided() {
+            return Err(NotDivided);
+        }
+        if !self.div_data().unwrap().is_free() {
+            return Err(Used);
+        }
+
+        self.is_divided = false;
+        unsafe { Ok(self.data.div_data) }
+    }
+}
+
 impl DivData {
 
     /// Count allocations of page by the index.
