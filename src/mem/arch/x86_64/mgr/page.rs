f28c4a84a77999613af04b2121822777fd22b7f3
Maxym Naumchyk <max.naumch@gmail.com>
2018-01-14 05:49:41 +0200

    Changed 'new' fn and added docs

--- src/mem/arch/x86_64/mgr/page.rs
+++ src/mem/arch/x86_64/mgr/page.rs
@@ -1,7 +1,7 @@
 use mem::Address;
 
 #[derive(Clone, Copy, PartialEq)]
-/// 2MiB page info.
+/// 2MiB page wrap.
 pub struct Page2m {
 
     /// Address of a page.
@@ -10,10 +10,25 @@ pub struct Page2m {
 
 impl Page2m {
 
-    pub fn new(addr: Address) -> Self {
+    /// Create new Page2m wrap without checking if given address is a
+    /// valid 2 MiB page address. Valid address must be aligned to
+    /// 2 MiB boundary.
+    pub unsafe fn new_unchecked(addr: Address) -> Self {
         Page2m { addr : addr }
     }
 
+    /// Create new Page2m wrap. None is returned if address is not aligned
+    /// and thus cannot point to a valid 2 MiB page.
+    pub fn new(addr: Address) -> Option<Self> {
+        if addr % (2048 * 1024 * 1024) != 0 {
+            None
+        } else {
+            let s = unsafe { Self::new_unchecked(addr) };
+            Some(s)
+        }
+    }
+
+    /// Address of a 2 MiB memory covered by this page wrapper.
     pub fn addr(&self) -> Address {
         self.addr
     }
